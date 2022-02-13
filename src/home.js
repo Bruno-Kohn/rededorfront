035441b3cf2d142530db7adf99eadbf07d6f177e
Bruno-Kohn <brunokohn1993@gmail.com>
2022-02-13 15:37:29 -0300

    feat: searching cep

--- src/home.js
+++ src/home.js
@@ -1,14 +1,40 @@
 import styled from 'styled-components';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [searchCep, setSearchCep] = useState('');
+  const [infos, setInfos] = useState('');
+
+  function searchCepRequest() {
+    const req = axios.post('http://localhost:4000/search', { cep: searchCep });
+    req.then((resp) => setInfos(resp.data));
+    req.catch((error) => console.log(error));
+  }
+
   return (
     <Container>
       <SearchBar
-        type='text'
+        type='number'
         placeholder='Input your CEP code here (ex: 12345000)'
-        //onChange={(e) => setSearchCards(e.target.value)}
+        onChange={(e) => setSearchCep(e.target.value)}
       ></SearchBar>
-      <CepButton>Send</CepButton>
+      <CepButton onClick={() => searchCepRequest()}>Send</CepButton>
+      <Card>
+        {infos === '' ? (
+          <h1>Enter a CEP code above for more information</h1>
+        ) : infos.status === 200 ? (
+          <>
+            <h1>CEP code: {infos.code}</h1>
+            <h1>State: {infos.state}</h1>
+            <h1>City: {infos.city}</h1>
+            <h1>District: {infos.district}</h1>
+            <h1>Address: {infos.address}</h1>
+          </>
+        ) : (
+          <h1>Please, type a valid CEP code</h1>
+        )}
+      </Card>
     </Container>
   );
 }
@@ -44,4 +70,22 @@ const CepButton = styled.button`
   border: none;
   margin-top: 20px;
   cursor: pointer;
+`;
+
+const Card = styled.div`
+  width: 500px;
+  min-height: 150px;
+  height: auto;
+  margin-top: 20px;
+  border-radius: 8px;
+  border: none;
+  background: RGBA(10, 192, 163, 0.36);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    margin: 10px;
+  }
 `;
